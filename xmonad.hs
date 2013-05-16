982bec5ae9d5450f73b9554e1b8a2ae9a2dbe272
nogal <shrimpcracker89@gmail.com>
2013-05-16 19:57:47 -0400

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,64 @@
+--Nogal's XMonad Setup
+--
+
+-- ============================Key Bindings=========================== --
+----------------------------Launch Application---------------------------
+-- Super + Space                     Application Launcher
+-- Super + F                         File Manager
+-- Super + W                         Web Client
+-- Super + M                         Mail Client
+-- Super + T                         Terminal
+
+-------------------------------Navigation--------------------------------
+-- Super + [1 - 9]			         Move to workspace N
+-- Super + Shift + [1 - 9]		     Move Window to workspace 
+
+--------------------------Window/Layout Management-----------------------
+-- Alt + H/L                         Shink/Expand Mater Area
+-- Alt + D/G                         Mirror Shrink/Expand
+-- Alt + ,/.                         Increment/Deincrement Master Area
+-- Alt + j/k                         Cycle active window  
+-- Alt + Shift + j/k                 Move Window along Window Cycle
+-- Alt + f                           Toggle Fullscreen
+-- Alt + m                           Focus Master Window
+-- Alt + Shift + C                   Kill Focused Window
+-- Alt + Click                       Pull Window to Floating Position
+-- Alt + t                           Tile Floating Window
+ 
+import XMonad
+
+import XMonad.Config.Desktop
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ToggleLayouts
+
+import XMonad.Util.EZConfig
+
+manageHook = manageDocks
+myLayout = avoidStruts $ smartBorders $ toggleLayouts Full (tiled) ||| Mirror tiled ||| noBorders Full  
+  where
+     tiled   = ResizableTall nmaster delta frac slaves
+     nmaster = 1
+     frac   = 1/2
+     delta   = 3/100
+     slaves = [] 
+
+main = xmonad $ ewmh desktopConfig 
+        { modMask = mod1Mask
+        , layoutHook = myLayout 
+        , handleEventHook = handleEventHook desktopConfig <+> docksEventHook <+> fullscreenEventHook }
+        -- `removeKeysP` [("M1-S-q")]
+         `additionalKeysP`
+         [ ("M1-d", sendMessage MirrorShrink)
+         , ("M1-g", sendMessage MirrorExpand)
+         , ("M1-f", sendMessage (Toggle "Full")) 
+         , ("M4-<Space>", spawn "synapse")
+         , ("M4-w", spawn "firefox")
+         , ("M4-m", spawn "thunderbird")
+         , ("M4-t", spawn "terminator")
+         , ("M4-f", spawn "nautilus")
+         ]
